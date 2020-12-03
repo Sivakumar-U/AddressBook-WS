d3b68c8cb00e7e4debd09403abffe8739937c64f
Sivakumar-U <usivakumarreddy@gmail.com>
2020-12-03 17:11:43 +0530

    Added: Code to edit a contact in address book

--- src/main/java/com/blz/addressbook/AddressBook.java
+++ src/main/java/com/blz/addressbook/AddressBook.java
@@ -7,7 +7,8 @@ import java.util.Scanner;
 public class AddressBook {
 
 	Scanner sc = new Scanner(System.in);
-	List<ContactDetails> contact = new ArrayList<ContactDetails>();
+
+	static List<ContactDetails> contact = new ArrayList<ContactDetails>();
 
 	public void addContact() {
 		System.out.println("Enter your firstName : ");
@@ -30,6 +31,62 @@ public class AddressBook {
 		ContactDetails obj = new ContactDetails(firstName, lastName, address, city, state, zip, phoneNo, email);
 		contact.add(obj);
 		System.out.println(contact);
+	}
+
+	public void editContact() {
+		System.out.println(" Enter the first name ");
+		String fName = sc.nextLine();
+		for (int i = 0; i < contact.size(); i++) {
+			if (contact.get(i).getFirstName().equals(fName)) {
+				System.out.println("index : " + contact.get(i));
+				Scanner input = new Scanner(System.in);
+				System.out.println(
+						"Choose option to edit:\n 1.first name\n 2.last name\n 3.city\n 4.state\n 5.zip\n 6.phone\n 7.email ");
+				int select = sc.nextInt();
+				switch (select) {
+				case 1:
+					System.out.println(" Enter first name ");
+					String firstName = input.nextLine();
+					contact.get(i).setFirstName(firstName);
+					break;
+				case 2:
+					System.out.println(" Enter last name ");
+					String lastName = input.nextLine();
+					contact.get(i).setLastName(lastName);
+					break;
+				case 3:
+					System.out.println(" Enter city name ");
+					String city = input.nextLine();
+					contact.get(i).setCity(city);
+					break;
+				case 4:
+					System.out.println(" Enter State ");
+					String state = input.nextLine();
+					contact.get(i).setState(state);
+					break;
+				case 5:
+					System.out.println(" Enter pincode ");
+					int zip = input.nextInt();
+					contact.get(i).setZip(zip);
+					break;
+				case 6:
+					System.out.println(" Enter Mobile number ");
+					long phone = input.nextLong();
+					contact.get(i).setPhoneNo(phone);
+					break;
+				case 7:
+					System.out.println(" Enter Email id ");
+					String email = input.nextLine();
+					contact.get(i).setEmail(email);
+					break;
+				default:
+					System.out.println(" Enter valid input ");
+					break;
+				}
+			}
+		}
+		System.out.println(contact);
+		System.out.println("*****Successfully edited****");
 	}
 
 }
--- src/main/java/com/blz/addressbook/AddressBookMain.java
+++ src/main/java/com/blz/addressbook/AddressBookMain.java
@@ -1,11 +1,41 @@
 package com.blz.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
 
-	static AddressBook addressBook = new AddressBook();
-
 	public static void main(String[] args) {
-		addressBook.addContact();
+
+		options();
+	}
+
+	static void options() {
+		AddressBook addressBook = new AddressBook();
+		Scanner sc = new Scanner(System.in);
+		int flag = 1;
+		while (flag == 1) {
+			System.out.println("Welcome to address book program ");
+			System.out.println("Choose options: \n1.AddContact \n2.EditContact \n3.Exit ");
+			int choice = sc.nextInt();
+			switch (choice) {
+			case 1:
+				addressBook.addContact();
+				break;
+			case 2:
+				if (AddressBook.contact.isEmpty()) {
+					System.out.println(" Address book is empty ");
+					break;
+				}
+				addressBook.editContact();
+				break;
+			case 3:
+				flag = 0;
+				System.out.println("Exit");
+				break;
+			default:
+				System.out.println("Invalid Choice");
+			}
+		}
 	}
 
 }
