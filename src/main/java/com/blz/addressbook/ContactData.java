cc7e18bd2117cd2737955abbe0b24a0444a6a63a
Sivakumar-U <usivakumarreddy@gmail.com>
2020-12-05 13:26:01 +0530

    Added: Equals method in contact data

--- src/main/java/com/blz/addressbook/ContactData.java
+++ src/main/java/com/blz/addressbook/ContactData.java
@@ -24,6 +24,21 @@ public class ContactData {
 		this.email = email;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + (int) (phoneNo ^ (phoneNo >>> 32));
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		result = prime * result + zip;
+		return result;
+	}
+
 	public String getFirstName() {
 		return firstName;
 	}
@@ -86,6 +101,52 @@ public class ContactData {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ContactData other = (ContactData) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (phoneNo != other.phoneNo)
+			return false;
+		if (state == null) {
+			if (other.state != null)
+				return false;
+		} else if (!state.equals(other.state))
+			return false;
+		if (zip != other.zip)
+			return false;
+		return true;
 	}
 
 	@Override
