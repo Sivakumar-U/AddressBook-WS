042dbb44d132bf9065a3b1efdb3d412d7f352ff2
Sivakumar-U <usivakumarreddy@gmail.com>
2020-12-05 15:36:51 +0530

    Added: Test code to read entries of address book from json server

--- src/test/java/com/blz/addressbook/AddressBookTest.java
+++ src/test/java/com/blz/addressbook/AddressBookTest.java
@@ -1,23 +1,43 @@
 package com.blz.addressbook;
 
+import static org.junit.Assert.assertEquals;
+
 import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.blz.addressbook.AddressBookService.IOService;
+import com.google.gson.Gson;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 
 public class AddressBookTest {
 
+	private static final IOService REST_IO = null;
 	private static AddressBookService addressBook;
 
 	@BeforeClass
 	public static void createAddressBookObj() {
 		addressBook = new AddressBookService();
 		System.out.println("Welcome to the Address Book System.. ");
+	}
+
+	@AfterClass
+	public static void nullObj() {
+		addressBook = null;
+	}
+	
+	@Before
+	public void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = 3000;
 	}
 
 	@Test
@@ -58,8 +78,7 @@ public class AddressBookTest {
 	@Test
 	public void givenMultipleContact_WhenAdded_ShouldSyncWithDB() throws AddressBookException {
 		ContactData[] contactArray = {
-				new ContactData("Tharun", "Pulluru", "Mcl", "Tirupati", "AP", 517536, 897565874,
-						"tharun@gmail.com"),
+				new ContactData("Tharun", "Pulluru", "Mcl", "Tirupati", "AP", 517536, 897565874, "tharun@gmail.com"),
 				new ContactData("Nani", "Kalthireddy", "Mr palli", "Tirupati", "AP", 517533, 987456320,
 						"nani@gmail.com") };
 		addressBook.addMultipleContactsToDBUsingThreads(Arrays.asList(contactArray));
@@ -69,5 +88,21 @@ public class AddressBookTest {
 		Assert.assertTrue(result2);
 
 	}
+	
+	private ContactData[] getAddressbookList() {
+		Response response = RestAssured.get("/addressBookWS");
+		System.out.println("Adddressbook entries in JsonServer :\n" + response.asString());
+		ContactData[] arrayOfPerson = new Gson().fromJson(response.asString(), ContactData[].class);
+		return arrayOfPerson;
+	}
+
+	@Test
+	public void givenAddressbookDataInJsonServer_WhenRetrieved_ShouldMatchCount() {
+		ContactData[] arrayOfPerson = getAddressbookList();
+		addressBook = new AddressBookService(Arrays.asList(arrayOfPerson));
+		long entries = addressBook.countEntries(REST_IO);
+		assertEquals(2, entries);
+	}
+
 
 }
